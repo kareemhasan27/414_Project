ec8f617ce648256e417c9fb1354ac20c5379c93d
Kareem A Hasan <keem27@umd.edu>
2025-04-26 23:11:43 -0400

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import os
+import plotly.io as pio
+
+df = pd.read_csv("sprint1data.csv")
+
+display(df.head())
+display(df.describe().info())
+df.isnull().sum()
+
